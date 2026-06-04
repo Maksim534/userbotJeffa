e369ad1b96059cdbca1eadab5a6d9a56b524efbb
Maksim534 <129356035+Maksim534@users.noreply.github.com>
2026-06-04 18:59:22 +0300

    Update ping.py

--- modules/ping.py
+++ modules/ping.py
@@ -1,1 +1,17 @@
+#!/usr/bin/env python3
+# userbot/modules/ping.py
+from loguru import logger
 
+__info__ = {
+    "name": "ping",
+    "version": "1.0",
+    "author": "catastrophe",
+    "description": "Проверка работы бота"
+}
+
+async def __setup__(client, commands_registry, handlers_registry):
+    commands_registry["ping"] = ping_command
+    logger.info("Модуль ping загружен")
+
+async def ping_command(event):
+    await event.reply("🏓 Pong! Бот работает, Альфа!")
